f24123b0fbd2db4dff8bd77dd70638e6c35a54d7
DuongHuuLuan <luanb2205946@student.ctu.edu.vn>
2025-09-27 23:46:51 +0700

    Cap nhat Danh sach nha xuat ban

--- src/App.vue
+++ src/App.vue
@@ -3,7 +3,11 @@ export default {};
 </script>
 
 <template>
-  <h1>Hello, Vue.js</h1>
+  <div id="app">
+    <div class="container mt-3">
+      <router-view />
+    </div>
+  </div>
 </template>
 
 <style>
